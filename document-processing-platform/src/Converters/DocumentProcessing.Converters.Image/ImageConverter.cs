173b40eb2e74c9123714f4363231c97d0e526ce4
seozdemir <seozdemir@N307488>
2026-07-28 23:12:29 +0300

    feat(conversion): introduce converter engine abstraction with shared contracts

---
+++ document-processing-platform/src/Converters/DocumentProcessing.Converters.Image/ImageConverter.cs
@@ -0,0 +1,17 @@
+﻿using DocumentProcessing.Contracts.Converter;
+
+namespace DocumentProcessing.Converters.Image
+{
+    public class ImageConverter
+    {
+        public ConverterResult Convert(ConverterRequest request)
+        {
+            return new ConverterResult
+            {
+                IsSuccessful = true,
+                Content = request.Content, // TODO: dönüştürülen byte
+                DocumentType = DocumentType.Pdf
+            };
+        }
+    }
+}
---
+++ document-processing-platform/src/Converters/DocumentProcessing.Converters.Office/OfficeConverter.cs
@@ -0,0 +1,17 @@
+﻿using DocumentProcessing.Contracts.Converter;
+
+namespace DocumentProcessing.Converters.Office
+{
+    public class OfficeConverter
+    {
+        public ConverterResult Convert(ConverterRequest request)
+        {
+            return new ConverterResult
+            {
+                IsSuccessful = true,
+                Content = request.Content, // TODO: dönüştürülen byte
+                DocumentType = DocumentType.Pdf
+            };
+        }
+    }
+}
